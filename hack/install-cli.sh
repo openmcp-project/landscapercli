8b6fafa3617575fc6f2a3f56b7e5adcb22e24f3a
Robert Graeff <robert.graeff@sap.com>
2025-11-19 14:02:24 +0100

    feat: publish and install

--- hack/install-cli.sh
+++ hack/install-cli.sh
@@ -10,9 +10,8 @@ PROJECT_ROOT="$(realpath $(dirname $0)/..)"
 
 GITTREESTATE=`([ -z "$(git status --porcelain 2>/dev/null)" ] && echo clean || echo dirty)`
 
-
 go install \
   -ldflags "-X github.com/openmcp-project/landscapercli/pkg/version.LandscaperCliVersion=$EFFECTIVE_VERSION \
             -X github.com/openmcp-project/landscapercli/pkg/version.gitTreeState=$GITTREESTATE \
             -X github.com/openmcp-project/landscapercli/pkg/version.gitCommit=$(git rev-parse --verify HEAD)" \
-  ${PROJECT_ROOT}/landscaper-cli
+  ${PROJECT_ROOT}/cmd/landscaper-cli
